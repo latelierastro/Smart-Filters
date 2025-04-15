b8ca897d2c7da470de405b265a6e64653f8ef9fd
Benoit Saintot <benoit.saintot@gmail.com>
2025-04-15 17:17:52 +0200

    FILTER WHEEL MODELISATION

--- PlanMyNightDockables/Views/PlanMyNightDockableView.xaml.cs
+++ PlanMyNightDockables/Views/PlanMyNightDockableView.xaml.cs
@@ -2,33 +2,38 @@
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace PlanMyNight.PlanMyNightDockables.Views {
     public partial class PlanMyNightDockableView : UserControl {
         public PlanMyNightDockableView() {
             InitializeComponent();
-            var vm = new PlanMyNightDockables.ViewModels.PlanMyNightDockableViewModel();
+            this.Loaded += OnLoaded;
+
+            var vm = new PlanMyNightDockableViewModel();
             this.DataContext = vm;
-
-            vm.ToastRequested += ShowToast;}
-
-           
-        private void CheckBox_Checked(object sender, System.Windows.RoutedEventArgs e) {
-
+            vm.ToastRequested += ShowToast;
         }
 
-        private void CheckBox_Checked_1(object sender, System.Windows.RoutedEventArgs e) {
-
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            if (ReferenceTextBlock.Foreground is SolidColorBrush textBrush) {
+                // Ajoute une ressource dynamique utilisable dans le XAML
+                this.Resources["FilterWheelStrokeBrush"] = new SolidColorBrush(textBrush.Color);
+            } else {
+                // Fallback : si la couleur n'est pas trouvée, applique le noir
+                this.Resources["FilterWheelStrokeBrush"] = new SolidColorBrush(Colors.Black);
+            }
         }
 
-        private void CheckBox_Checked_2(object sender, System.Windows.RoutedEventArgs e) {
 
-        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e) { }
 
-        private void CheckBox_Checked_3(object sender, System.Windows.RoutedEventArgs e) {
+        private void CheckBox_Checked_1(object sender, RoutedEventArgs e) { }
 
-        }
+        private void CheckBox_Checked_2(object sender, RoutedEventArgs e) { }
 
+        private void CheckBox_Checked_3(object sender, RoutedEventArgs e) { }
 
         private void OnCalculateClicked(object sender, RoutedEventArgs e) {
             if (DataContext is PlanMyNightDockableViewModel vm) {
@@ -37,21 +42,20 @@ namespace PlanMyNight.PlanMyNightDockables.Views {
             }
         }
 
-
         private void OnLoadProfileClicked(object sender, RoutedEventArgs e) {
-            if (DataContext is PlanMyNightDockables.ViewModels.PlanMyNightDockableViewModel vm) {
+            if (DataContext is PlanMyNightDockableViewModel vm) {
                 vm.OnLoadProfileClicked();
             }
         }
 
         private void OnSaveProfileClicked(object sender, RoutedEventArgs e) {
-            if (DataContext is PlanMyNightDockables.ViewModels.PlanMyNightDockableViewModel vm) {
+            if (DataContext is PlanMyNightDockableViewModel vm) {
                 vm.OnSaveProfileClicked();
             }
         }
 
         private void OnDeleteProfileClicked(object sender, RoutedEventArgs e) {
-            if (DataContext is PlanMyNightDockables.ViewModels.PlanMyNightDockableViewModel vm) {
+            if (DataContext is PlanMyNightDockableViewModel vm) {
                 vm.OnDeleteProfileClicked();
             }
         }
@@ -64,6 +68,5 @@ namespace PlanMyNight.PlanMyNightDockables.Views {
 
             ToastNotification.Visibility = Visibility.Collapsed;
         }
-
     }
 }
