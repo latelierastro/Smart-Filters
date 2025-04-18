5725cb5559657cfcb359e0e0f7034efee9091649
Benoit SAINTOT <benoit.saintot@gmail.com>
2025-04-19 00:24:04 +0200

    Ajout coche Flip

---
+++ Converters/SegmentCornerRadiusConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PlanMyNight.Converters {
+    public class SegmentCornerRadiusConverter : IMultiValueConverter {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+            var item = values[0];
+            var itemsControl = values[1] as System.Windows.Controls.ItemsControl;
+
+            if (item == null || itemsControl == null)
+                return new CornerRadius(0);
+
+            int index = itemsControl.Items.IndexOf(item);
+            int totalCount = itemsControl.Items.Count;
+
+            if (index == 0) // Premier segment
+                return new CornerRadius(6, 0, 0, 6);
+            else if (index == totalCount - 1) // Dernier segment
+                return new CornerRadius(0, 6, 6, 0);
+            else
+                return new CornerRadius(0); // Segments intermédiaires
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
